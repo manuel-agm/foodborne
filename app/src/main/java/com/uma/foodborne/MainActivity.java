c95abcb6578e6ebef32bb66ae7a3d0ec8b85e0a9
Manuel Cristóbal Roldán Gómez <xdmrgxd@uma.es>
2022-01-08 23:34:47 +0100

    Primera actividad casi terminada

--- app/src/main/java/com/uma/foodborne/MainActivity.java
+++ app/src/main/java/com/uma/foodborne/MainActivity.java
@@ -1,14 +1,192 @@
 package com.uma.foodborne;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.app.ActionBar.LayoutParams;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.uma.foodborne.adapters.RecipesAdapter;
+import com.uma.foodborne.models.Receta;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private EditText txtSearch;
+    private EditText txtPages;
+    private Button btnSearch;
+    private Button btnPrev;
+    private Button btnNext;
+    private Button btnGo;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private CheckBox checkVegetariano;
+    private CheckBox checkVegano;
+    private CheckBox checkGluten;
+    private CheckBox checkLactosa;
+
+    private final int NUM_RECETAS = 5;
+
+    private int offset = 0; //Numero de items cargados
+
+    private LinearLayoutManager layoutManager;
+    private RecipesAdapter recipesAdapter;
+
+    private int actualMod = -1;
+    private String actualInformacion = "";
+
+    private int paginaActual = 1;
+    private int paginasTotales = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            StrictMode.enableDefaults();
+
+        txtSearch = (EditText) findViewById(R.id.txtSearch);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+
+        btnPrev = (Button) findViewById(R.id.btnPre);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        txtPages = (EditText) findViewById(R.id.txtPages);
+
+
+        checkVegetariano = (CheckBox) findViewById(R.id.checkVegetariano);
+        checkVegano = (CheckBox) findViewById(R.id.checkVegano);
+        checkGluten = (CheckBox) findViewById(R.id.checkGluten);
+        checkLactosa = (CheckBox) findViewById(R.id.checkLactosa);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recicler);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+
+            @Override
+            public void onRefresh() {
+
+                offset = 0;
+                actualizarRecycler(actualMod, actualInformacion, offset);
+                swipeRefreshLayout.setRefreshing(false);
+
+            }
+
+        });
+
+        btnPrev.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offset -= 5;
+                actualizarRecycler(actualMod, actualInformacion, offset);
+            }
+        });
+
+        btnNext.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                offset += 5;
+                actualizarRecycler(actualMod, actualInformacion, offset);
+            }
+        });
+
+        btnGo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((txtPages.getText() == null || txtPages.getText().toString() == "" || Integer.parseInt(txtPages.getHint().toString().split("/")[1]) < Integer.parseInt(txtPages.getText().toString()))){
+                   Toast.makeText(v.getContext(), "Introduzca número de página válido", Toast.LENGTH_SHORT).show();
+                } else {
+                    offset = Integer.parseInt(txtPages.getText().toString()) * NUM_RECETAS;
+                    actualizarRecycler(actualMod, actualInformacion, offset);
+                }
+            }
+        });
+
+
+        checkVegetariano.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkVegano.setChecked(false);
+            }
+        });
+
+        checkVegano.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkVegetariano.setChecked(false);
+            }
+        });
+
+        btnSearch.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualMod = 0;
+                actualInformacion = "";
+                actualInformacion += "query=" + txtSearch.getText();
+                if(checkVegetariano.isChecked()) actualInformacion += "&diet=vegetarian";
+                if(checkVegano.isChecked()) actualInformacion += "&diet=vegetarian";
+                if(checkGluten.isChecked() || checkLactosa.isChecked()) {
+                    actualInformacion += "&intolerances=";
+
+                    if(checkLactosa.isChecked()) actualInformacion += "dairy,";
+                    if(checkGluten.isChecked()) actualInformacion += "gluten,";
+
+                    actualInformacion.substring(0, actualInformacion.lastIndexOf(','));
+                }
+
+                Toast myToast = Toast.makeText( v.getContext(), "https://api.spoonacular.com/recipes/complexSearch?" + actualInformacion, Toast.LENGTH_SHORT);
+                myToast.show();
+                offset = 0;
+                actualizarRecycler(actualMod, actualInformacion, offset);
+                //  https://api.spoonacular.com/recipes/complexSearch?query=pasta
+
+            }
+        });
+
+        actualizarRecycler(-1, "", 0);
+    }
+
+    // mode
+    // Search Recipe by Name and Filter: 0
+    // Random:                           default
+    private void actualizarRecycler(int mode, String information, int offset){
+        RecyclerView rvRecipes = (RecyclerView) findViewById(R.id.recicler);
+        rvRecipes.removeAllViews();
+        ArrayList<Receta> recipes = null;
+        try {
+            recipes = Receta.createRecipesList(NUM_RECETAS, mode, offset, information, this);
+        } catch (Exception  e) {
+            Toast myToast = Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_LONG);
+            myToast.show();
+            e.printStackTrace();
+        }
+        // Create adapter passing in the sample user data
+        recipesAdapter = new RecipesAdapter(recipes);
+        // Attach the adapter to the recyclerview to populate items
+        rvRecipes.setAdapter(recipesAdapter);
+        // Set layout manager to position the items
+        layoutManager = new LinearLayoutManager(this);
+        rvRecipes.setLayoutManager(layoutManager);
+        // That's all!
     }
 }
---
+++ app/src/main/java/com/uma/foodborne/models/Receta.java
@@ -0,0 +1,142 @@
+package com.uma.foodborne.models;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.uma.foodborne.MainActivity;
+import com.uma.foodborne.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Receta {
+    private int id;
+    private String title;
+    private String image;
+
+    private static final String API_KEY = "65757ee1a05f42769b1ec419f5a10cb2";
+    private static final int MODE_SEARCH = 0;
+
+    private static String respuesta = "";
+    private static int offset = 0;
+
+    public Receta(int id, String title, String image){
+        this.id = id;
+        this.title = title;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Receta> createRecipesList(int numRecipes, int mode, int offset, String information, MainActivity mainActivity) throws JSONException, InterruptedException {
+        ArrayList<Receta> recipes = new ArrayList<Receta>();
+
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        String jsonArrayName = "";
+
+        switch(mode){
+            case MODE_SEARCH: // Search Recipe by Name and Filter
+                jsonArrayName = "results";
+                break;
+
+            default: // Random
+                jsonArrayName = "recipes";
+        }
+        getRecipes(5, information, mode, offset, countDownLatch);
+
+        countDownLatch.await();
+
+        JSONObject rootObj = new JSONObject(respuesta);
+
+        JSONArray recipesJSON = rootObj.getJSONArray(jsonArrayName);
+
+        for(int i = 0; i <numRecipes;i++) {
+            JSONObject object = recipesJSON.getJSONObject(i);
+
+            recipes.add(new Receta(object.getInt("id"), object.getString("title"), object.getString("image").split("/")[object.getString("image").split("/").length-1]));
+        }
+
+        if(mode == MODE_SEARCH){
+            EditText txtPages = (EditText) mainActivity.findViewById(R.id.txtPages);
+            txtPages.setHint(((int)Math.floor(offset/numRecipes)+1 + "/" + (int)Math.round(rootObj.getInt("totalResults")/numRecipes)));
+        }
+
+        //https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert&apiKey=65757ee1a05f42769b1ec419f5a10cb2
+
+        /*for(int i = 1; i <=numRecipes;i++) {
+            Receta recipe = new Receta(i, "aguacate", 10*i, "Cajun-Spiced-Black-Bean-and-Sweet-Potato-Burgers-227961.jpg");
+            recipes.add(recipe);
+        }*/
+
+        return recipes;
+    }
+
+    private static void getRecipes(int numRecipes, String information, int mode, int offset, CountDownLatch countDownLatch){
+        String modeString;
+        switch (mode) {
+            case MODE_SEARCH: modeString = "complexSearch";
+            break;
+            default: modeString = "random";
+        }
+
+        if(offset > 0){
+            information += "&offset=" + offset;
+        }
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.spoonacular.com/recipes/" + modeString + "?number=" + numRecipes + "&" + information + "&apiKey=" + API_KEY)
+                .get()
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                respuesta = "";
+                e.printStackTrace();
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, Response response) throws IOException {
+
+                respuesta = response.body().string();
+                response.close();
+                countDownLatch.countDown();
+            }
+        });
+    }
+}
